87e3c58956bf8dae895c514e96d0a678116f31cd
Arthur D'Avila <arthurdavila879@gmail.com>
2026-03-19 10:36:08 -0300

    Update Cliente.java

--- ContaCliente/src/main/java/com/mycompany/contacliente/Cliente.java
+++ ContaCliente/src/main/java/com/mycompany/contacliente/Cliente.java
@@ -32,24 +32,5 @@ public class Cliente {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-    
+  
 }
